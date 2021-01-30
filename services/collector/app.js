a32cd94a0aacee1abd5d565b8c62a3d9d43c834c
Adrian De Lisle <adriandelisle@gmail.com>
2021-01-29 22:52:12 -0800

    Basic collector with mock data

--- services/collector/app.js
+++ services/collector/app.js
@@ -1,9 +1,20 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = process.env.PORT ?? 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+function randomNumber(min, max) {
+  return Math.floor(Math.random() * (max - min) + min);
+}
+
+app.get("/am-i-up", (req, res) => {
+  res.json("Ok");
+});
+
+app.get("/infected/city/:city/", (req, res) => {
+  res.json({
+    "source-a": randomNumber(400, 1000),
+    "source-b": randomNumber(400, 1000),
+  });
 });
 
 app.listen(port, () => {
