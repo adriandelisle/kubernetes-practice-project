3183259b6514ab74cc8ac978e9c45d8fe5411b9f
Adrian De Lisle <adriandelisle@gmail.com>
2021-01-31 00:33:21 -0800

    Basic aggregator with mock data

--- services/aggregator/app.js
+++ services/aggregator/app.js
@@ -1,9 +1,38 @@
 const express = require("express");
+const axios = require("axios");
+
 const app = express();
 const port = process.env.PORT ?? 3000;
 
+const collector_url = process.env.COLLECTOR_URL ?? "localhost:3000";
+
 app.get("/am-i-up", (req, res) => {
   res.json("Ok");
+});
+
+app.get("/average/city/:city", async (req, res) => {
+  try {
+    const { city } = req.params;
+
+    const { data } = await axios.get(
+      `http://${collector_url}/infected/city/${city}`
+    );
+    const sources = Object.keys(data);
+    const total = Object.values(data).reduce((acc, curr) => acc + curr, 0);
+
+    const average = Math.ceil(total / sources.length);
+
+    const responseData = {
+      city: city,
+      infected: average,
+      time: new Date().toISOString(),
+      sources: sources,
+    };
+
+    res.json(responseData);
+  } catch (e) {
+    console.error(e);
+  }
 });
 
 app.listen(port, () => {
